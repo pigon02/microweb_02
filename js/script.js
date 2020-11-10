22dd476470d9f6bb655e5dc9381d77e0303799d4
junyoung <pigon02@naver.com>
2020-11-10 17:11:03 +0900

    microweb_02 first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+
+    var sec01_slider = $('.sec01_slider');
+
+
+
+
+
+    sec01_slider.slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        autoplay:3000,
+        arrows:false
+        
+    });
+
+
+
+
+
+
+});
